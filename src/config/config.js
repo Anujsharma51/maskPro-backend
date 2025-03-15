b6ee0afd09903499f7947e6051c010344d7e5b26
anuj-s <anuujsharma96@gmail.com>
2025-03-16 00:25:33 +0530

    mongodata base url added

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 module.exports = (function () {
   return {
     PORT: "5000",
+    url:"mongodb+srv://anuj:<Anuj@199>@learning.i6tfk.mongodb.net/?retryWrites=true&w=majority&appName=Learning"
   };
 })();
